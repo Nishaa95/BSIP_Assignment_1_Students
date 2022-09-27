8c7bf2984d5122bb1eeec2a248e019288ff46089
Nishaa95 <nishaamkce@gmail.com>
2022-09-27 16:37:32 -0600

    update

--- thinkstats2.py
+++ thinkstats2.py
@@ -3039,3 +3039,15 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+    def CohenEffectSize(group1, group2):
+    
+        diff = group1.mean() - group2.mean()
+
+        var1 = group1.var()
+        var2 = group2.var()
+        n1, n2 = len(group1), len(group2)
+
+        pooled_var = (n1 * var1 + n2 * var2) / (n1 + n2)
+        d = diff / np.sqrt(pooled_var)
+        return d
